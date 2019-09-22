8738474dd82fd0ba670a090097ba682754d640d7
John Stableford <john.stableford@digio.com.au>
2019-09-22 10:12:20 +1000

    chore: srcSets

---
+++ sim-customer-acquisition/src/SimpleProducer.test.kt
@@ -0,0 +1,17 @@
+package com.sim.customer.acquisition;
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SimpleTest {
+    @Test
+    fun testEqualsTrue(){
+        var b=true
+        assertEquals(true,b)
+    }
+    @Test
+    fun testEqualsFalse(){
+        var b=false
+        assertEquals(false,b)
+    }
+}
